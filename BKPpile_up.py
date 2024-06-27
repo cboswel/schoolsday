fb43eb2666617ef8a7da45bd060d3c86bb0e8e6f
Charlie <charlie.boswell11@gmail.com>
2024-06-27 16:58:30 +0100

    working!

--- BKPpile_up.py
+++ BKPpile_up.py
@@ -2,12 +2,13 @@
 
 import random
 import pdb
-import matplotlib.pyplot as plt
+import timeit
 
 MAX_GROUP_SIZE = 40
 MIN_GROUP_SIZE = 35
-AVAILABLE_SPOTS = 16
-THOROUGHNESS = 50000
+AVAILABLE_SPOTS = 8
+THOROUGHNESS = 1
+TEST_REPS = 250
 NUMBER_OF_GROUPS = 200
 
 class Group:
@@ -19,16 +20,16 @@ class Group:
 
 class Board:
     def __init__(self, sizes):
-        self.remainings = list(sizes)
-        self.groups = [[] for _ in range(len(self.remainings))]
+        self.remainings = remainings
+        self.groups = [[] * len(remainings)]
         self.score = 0
 
 def generate_groups(number_of_groups):
     groups = []
     for i in range(number_of_groups):
-        size = random.randint(5, MAX_GROUP_SIZE)
+        size = random.randint(1, MAX_GROUP_SIZE)
         score = random.randint(1, 10)
-        slots = random.sample(range(AVAILABLE_SPOTS), 3)
+        slots = random.sample(range(16), 3)
         group = Group([i], size, score, slots)
         groups.append(group)
     return groups
@@ -64,61 +65,71 @@ def combine(groups, newGroups):
 def combine(groups, board):
     delete_pile = []
     for group in groups:
-        if group.size > MAX_GROUP_SIZE:
+        if groups[0].size > MAX_GROUP_SIZE:
             raise Exception(f"Group of length {groups[0].size} found, groups must be under {MAX_GROUP_SIZE}.")
         if dont_skip():
-            max_slot = 0
-            for slot in group.slots:
-                if board.remainings[slot] > max_slot:
-                    max_slot = slot
-            slot = max_slot
+            slot = max(board.remainings[group.slots])
             if board.remainings[slot] >= group.size:
                 board.remainings[slot] -= group.size
                 board.score += group.score
-                board.groups[slot].append(group.size)
-            delete_pile.append(group)
+                delete_pile.append(group)
     for group in delete_pile:
         groups.remove(group)
     return 0
-
+            
 def pile_up(startingGroups):
-    board = Board([MAX_GROUP_SIZE] * AVAILABLE_SPOTS)
+    board = Board([40] * AVAILABLE_SPOTS)
     groups = sorted(startingGroups, key=lambda x: x.score, reverse=True)
     while groups != []:
         combine(groups, board)
-    enriched_kids = 0
-    for group in board.groups:
-        enriched_kids += sum(group)
-    board.score = board.score * enriched_kids
     return board
 
-def test_once():
+def test_once(tops):
+    top_score = 0
+    top_configuration = []
     startingGroups = generate_groups(NUMBER_OF_GROUPS)
-    best_board = Board([0])
     for i in range(THOROUGHNESS):
         board = pile_up(startingGroups)
-        if board.score > best_board.score:
-            best_board = board
-    return best_board
+        delete_pile = []
+        for leftover in board.remainings:
+            if leftover > (MIN_GROUP_SIZE):
+                delete_pile.append(group)
+        for group in delete_pile:
+            groups.remove(group)
+        groups = sorted(groups, key=lambda x: x.score, reverse=True)
+        iteration_score = 0
+        for slot in range(AVAILABLE_SPOTS):
+            slot_scores = []
+            for group in groups:
+                if slot == group.slots:
+                    slot_scores.append(group.score)
+                  #  print(f"potential group of {group.size}")
+            print(f"slot {slot} score = {max(slot_scores)} and ")
+            iteration_score += max(slot_scores)
+            if iteration_score > top_score:
+                top_score = iteration_score
+                tops.append(top_score)
+
 
 # TODO: Make it so it doesn't error when there's an empty slot. Instead, try other configurations first. Score it so an empty slot adds zero - that way that configuarion is probably skipped.
-def test_set(end):
-    global THOROUGHNESS
-    results = []
-    while THOROUGHNESS < end:
-        board = test_once()
-        results.append(board.score)
-        THOROUGHNESS += 1
-        print(THOROUGHNESS)
-    x = range(len(results))
-    y = results
-    fig, ax = plt.subplots()
-    ax.plot(x, y)
-    plt.show()
+def test_set(tops):
+    for _ in range(TEST_REPS):
+        test_once(tops)
+    avg = sum(tops) / len(tops)
+    return avg
 
 if __name__ == "__main__":
-    board = test_once()
-    for group in board.groups:
-        print(sum(group))
+    tops = []
+    test_once(tops)
+    x = []
+    y = []
+    times = []
+    for i in range(10, 100):
+        tops = []
+        THOROUGHNESS = i * 10
+       # times.append(timeit.timeit("y.append(test_set(tops))", globals=globals()))
+        y.append(test_set(tops))
+        x.append(THOROUGHNESS)
+        print(f"thoroughness of {THOROUGHNESS} yields {y[i-10]}")
     pdb.set_trace()
-
+        
--- sort.py
+++ sort.py
@@ -1,8 +1,38 @@
 #!/usr/bin/env python3
 
-from deprivation_by_postcode import *
+#from deprivation_by_postcode import *
 import pandas as pd
 import pgeocode
+import pdb
+import random
+
+#TODO: Types are backwardsly sorted, weightings? CSV output. Infinite score?
+
+MAX_GROUP_SIZE = 42
+MIN_GROUP_SIZE = 35
+AVAILABLE_SPOTS = 16
+THOROUGHNESS = 10000000
+
+class Group:
+    def __init__(self, size, score, possible_slots, name, email, postcode, types):
+        self.size = size
+        self.score = score
+        self.slots = possible_slots
+        self.name = name
+        self.assigned_slot = None
+        self.email = email
+        self.postcode = postcode
+        self.types = types
+
+class Board:
+    def __init__(self, dates):
+        self.remainings = {}
+        self.sizes = {}
+        self.groups = []
+        for date in dates:
+            self.remainings[date] = MAX_GROUP_SIZE
+            self.sizes[date] = []
+        self.score = 0
 
 def get_best_distances(postcodes):
     STEP_postcode = "DN22"
@@ -30,44 +60,192 @@ def get_best_distances(postcodes):
         best_scores.append(best)
     return best_scores
 
-def get_gender_scores(genders):
+def get_gender_scores(types):
     scores = []
-    for gender in genders:
-        if gender == "female":
+    for gender in types:
+        if "boys" in gender:
             score = 1
-        elif gender == "male":
-            score = -1
-        else:
-            score = 0
-        scores.append(score)
-    return scores
-        
-def get_type_scores(types):
-    scores = []
-    for school_type in types:
-        if school_type == "utc":
-            score = 1
-        elif school_type == "private":
+        elif "girls" in gender:
             score = -1
         else:
             score = 0
         scores.append(score)
     return scores
 
+def get_type_scores(types, names):
+    scores = []
+    for i, school_type in enumerate(types):
+        if "UTC" in school_type or "UTC" in names[i]:
+            score = -1
+        elif "Independent" in school_type:
+            score = 1
+        else:
+            score = 0
+        #print(f"{school_type} given a score of {score}")
+        scores.append(score)
+    return scores
+
+def FSM_scores(FSM):
+    scores = []
+    for percentage in FSM:
+        percentage = str(percentage).replace("%", "")
+        if percentage == '?':
+            scores.append(0)
+        else:
+            try:
+                percentage = float(percentage)
+            except:
+                print(f"Bad data point {percentage} in FSM")
+            if percentage > 40:
+                score = 40
+            elif percentage < 10:
+                score = 0
+            else:
+                score = percentage
+            score = len(FSM) - ((score / 40) * len(FSM)) # get a score out of <number of schools>
+            scores.append(score)
+    return scores
+
+def possible_date(dates):
+    slots = []
+    for date in dates:
+        if date not in slots:
+            slots.append(date)
+    return slots
+
+def date_parse(dates):
+    date_list = []
+    for date in dates:
+        if date[-1] == ";":
+            date = date[:-1]
+        date_list.append(date.split(";"))
+    return date_list
+
+def dont_skip():
+    percentage_chance = 50
+    if random.randint(0, 100) < percentage_chance:
+        return True
+    else:
+        return False
+
+def combine(groups, board):
+    delete_pile = []
+    for group in groups:
+        if group.size > MAX_GROUP_SIZE:
+            raise Exception(f"Group of length {groups[0].size} found, groups must be under {MAX_GROUP_SIZE}.")
+        if dont_skip():
+            max_remaining = 0
+            best_slot = group.slots[0]
+            for slot in group.slots:
+                if board.remainings[slot] > max_remaining:
+                    max_remaining = board.remainings[slot]
+                    best_slot = slot
+            slot = best_slot
+            if board.remainings[slot] >= group.size:
+                board.remainings[slot] -= group.size
+                board.score += group.score
+                board.sizes[slot].append(group.size)
+                group.assigned_slot = slot
+                board.groups.append(group)
+            delete_pile.append(group)
+    for group in delete_pile:
+        groups.remove(group)
+    return 0
+
+def pile_up(startingGroups, board):
+    groups = sorted(startingGroups, key=lambda x: x.score, reverse=True)
+    while groups != []:
+        combine(groups, board)
+    enriched_kids = 0
+    for date in board.sizes.values():
+        enriched_kids += sum(date)
+    board.score = board.score * enriched_kids
+    if board.score == 'inf':
+        pdb.set_trace()
+    return board
+
+def test_once():
+    startingGroups = generate_groups(NUMBER_OF_GROUPS)
+    best_board = Board([0])
+    for i in range(THOROUGHNESS):
+        board = pile_up(startingGroups)
+        if board.score > best_board.score:
+            best_board = board
+    return best_board
+
+
 if __name__ == "__main__":
-    data = pd.read_excel("schools.xlsx")
-    data["deprivation"] = get_deprivation(data["postcode"])
+    data = pd.read_excel("data.xlsx")
+#    data["deprivation"] = get_deprivation(data["postcode"])
     data["best_distance"] = get_best_distances(data["postcode"])
-    data["genders"] = get_gender_scores(data["gender"])
-    data["types"] = get_type_scores(data["type"])
+    data["genders"] = get_gender_scores(data["type"])
+    data["types"] = get_type_scores(data["type"], data["name"])
     data["distance_rank"] = data["best_distance"].rank()
-    data["deprivation_rank"] = data["deprivation"].rank()
+    data["FSM_scores"] = FSM_scores(data["FSM"])
+#    data["deprivation_rank"] = data["deprivation"].rank()
     data["gender_rank"] = data["genders"].rank(ascending=False)
     data["type_rank"] = data["types"].rank(ascending=False)
     deprivation_scaled = 1
-    data["priority"] = data["distance_rank"] + data["deprivation_rank"]
+    data["priority"] = data["distance_rank"] + data["FSM_scores"] + data["type_rank"] + data["gender_rank"] # + data["deprivation_rank"]
+    data["dates"] = date_parse(data["dates"])
+    #pdb.set_trace()
+    """
+    print("type:")
+    print(data.sort_values("type_rank")[["name", "type", "type_rank"]])
+    print("gender:")
+    print(data.sort_values("gender_rank")[["name", "type", "gender_rank"]])
+    print("FSM:")
+    print(data.sort_values("FSM_scores")[["name", "FSM", "FSM_scores"]])
+    """
+    data = data.sort_values("priority")
+    #print(data[["name", "distance_rank", "priority", "type_rank", "gender_rank"]])
 
-    print(data.sort_values("priority"))
+    groups = []
+    for index, group in data.iterrows():
+        try:
+            int(group["priority"])
+        except:
+            pdb.set_trace()
+        groups.append(Group(group["numbers"], group["priority"], group["dates"], group["name"], group["email"], group["postcode"], group["type"]))
+    possible_dates = []
+    for date_list in data["dates"]:
+        for date in date_list:
+            if date not in possible_dates:
+                possible_dates.append(date)
+    board = Board(possible_dates)
+
+    best_board = Board(["1st Jan"])
+    for i in range(THOROUGHNESS):
+        new_board = pile_up(groups, board)
+        if new_board.score > best_board.score:
+            best_board = new_board
+
+
+    output_data = {"name" : [], "size" : [], "email" : [], "postcode" : [], "assigned_slot" : []}
+    cut_data = {"name" : [], "type" : [], "size" : [], "email" : [], "postcode" : []}
+    for group in best_board.groups:
+        if group.name not in output_data["name"]:
+            output_data["name"].append(group.name)
+            output_data["size"].append(group.size)
+            output_data["email"].append(group.email)
+            output_data["postcode"].append(group.postcode)
+            output_data["assigned_slot"].append(group.assigned_slot)
+
+    df =  pd.DataFrame(output_data)
+    df = df.sort_values("assigned_slot")
+    name_list = []
+    for name in df["name"]:
+        name_list.append(name)
+    for index, group in data.iterrows():
+        if group["name"] not in name_list:
+            cut_data["name"].append(group["name"])
+            cut_data["size"].append(group["numbers"])
+            cut_data["email"].append(group["email"])
+            cut_data["postcode"].append(group["postcode"])
+            cut_data["type"].append(group["type"])
+    cut_df = pd.DataFrame(cut_data)
+    df.to_csv('out.csv', index=False)
+    cut_df.to_csv('rej.csv', index=False)
 
 
 """
